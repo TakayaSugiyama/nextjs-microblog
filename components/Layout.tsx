d29d6eb57e0433c7b436c06ef7d03b743479d929
takayasugiyama <s.takaya1027@gmail.com>
2022-12-02 20:28:24 +0900

    add css

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import HEAD from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/util.module.css";
+
+export const siteTitle = "Next.js Blog";
+const Layout = ({ children }: any) => {
+  const name = "Shin Code";
+  return (
+    <>
+      <HEAD>
+        <link rel="icon" href="/favicon.ico" />
+        <header className={styles.header}>
+          <Image
+            src="/images/profile.png"
+            alt="犬"
+            width={50}
+            height={50}
+            className={utilStyles.borderCircle}
+          />
+          <h1 className={utilStyles.heading2Xl}>{name}</h1>
+        </header>
+      </HEAD>
+      <main>{children}</main>
+    </>
+  );
+};
+export default Layout;
